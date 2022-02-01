75aa5839b38e718dbeb5572d6f4b8a1168fa4f98
Sumit K <synergy2411@outlook.com>
2022-02-01 14:35:29 +0530

    ngfor added

--- users-app/src/app/model/mocks.ts
+++ users-app/src/app/model/mocks.ts
@@ -1,6 +1,6 @@
 import { IUser } from './user.model';
 
-export const USER_DATA: IUser = {
+export const USER_DATA: Array<IUser> = [{
   firstName: 'bill',
   lastName: 'gates',
   dob: new Date('Dec 21, 1965'),
@@ -9,4 +9,22 @@ export const USER_DATA: IUser = {
   income: 50000,
   imagePath: './assets/images/bill.jpg',
   votes: 120,
-};
+},{
+  firstName: 'steve',
+  lastName: 'jobs',
+  dob: new Date('Jan 1, 1965'),
+  isWorking: false,
+  company: 'Apple',
+  income: 0,
+  imagePath: './assets/images/steve.jpg',
+  votes: 180,
+},{
+  firstName: 'tim b.',
+  lastName: 'lee',
+  dob: new Date('Aug 5, 1965'),
+  isWorking: true,
+  company: 'World Wide Web',
+  income: 30000,
+  imagePath: './assets/images/tim.jpg',
+  votes: 90,
+}];
