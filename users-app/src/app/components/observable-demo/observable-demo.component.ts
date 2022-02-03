18dc5fc82edf091fb7d18b6aba521d7b43d25a0b
Sumit K <synergy2411@outlook.com>
2022-02-03 11:57:06 +0530

    creational operator added

--- users-app/src/app/components/observable-demo/observable-demo.component.ts
+++ users-app/src/app/components/observable-demo/observable-demo.component.ts
@@ -1,47 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Observable, Subscription, from, interval, of, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-observable-demo',
   templateUrl: './observable-demo.component.html',
-  styleUrls: ['./observable-demo.component.css']
+  styleUrls: ['./observable-demo.component.css'],
 })
 export class ObservableDemoComponent implements OnInit {
-
   obs$ = new Observable((observer) => {
-    setTimeout(()=>{
-      observer.next("First Package")
-    },2000)
     setTimeout(() => {
-      observer.next("Second Package")
+      observer.next('First Package');
+    }, 2000);
+    setTimeout(() => {
+      observer.next('Second Package');
     }, 4000);
     setTimeout(() => {
       // observer.error(new Error("Something bad happened here"))
     }, 5000);
     setTimeout(() => {
-      observer.next("Third Package")
+      observer.next('Third Package');
     }, 6000);
     setTimeout(() => {
-      observer.complete()
+      observer.complete();
     }, 8000);
-  })
+  });
 
-  unSub$ : Subscription;
+  unSub$: Subscription;
 
-  onSubscribe(){
-    this.unSub$ = this.obs$.subscribe({
-      next : data => console.log(data),
-      error : err => console.log(err),
-      complete : () => console.log("[COMPLETED]")
-    })
+  fruits = ["apple", "banana", "grapes", "guava"]
+
+  fruits$ = from(this.fruits)
+
+  of$ = of('Hello World', 32, 'test@testing.com', true)
+
+  interval$ = interval(1000);
+
+  fromEvent$ = fromEvent(document, "click")
+
+  onSubscribe() {
+    console.clear();
+    this.fromEvent$.subscribe(console.log)
+
+    // this.fruits$.subscribe(console.log);
+    // this.unSub$ = this.interval$.subscribe(console.log);
+    // this.of$.subscribe(console.log)
+
+
+    // this.unSub$ = this.obs$.subscribe({
+    //   next: (data) => console.log(data),
+    //   error: (err) => console.log(err),
+    //   complete: () => console.log('[COMPLETED]'),
+    // });
   }
 
-  onUnsubscribe(){
+  onUnsubscribe() {
     this.unSub$.unsubscribe();
   }
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
