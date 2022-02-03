6d942985c5d5173074c0401b9bd42dd87a3c82cc
Sumit K <synergy2411@outlook.com>
2022-02-03 14:30:30 +0530

    basic SPA added

--- users-app/src/app/app.module.ts
+++ users-app/src/app/app.module.ts
@@ -1,33 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
-import { UserInfoComponent } from './components/users/user-info/user-info.component';
-import { UserImgComponent } from './components/users/user-img/user-img.component';
-import { LifeCycleDemoComponent } from './components/life-cycle-demo/life-cycle-demo.component';
-import { HighlightDirective } from './directives/highlight.directive';
-import { PipeDemoComponent } from './components/pipe-demo/pipe-demo.component';
-import { CountryCodePipe } from './pipes/country-code.pipe';
-import { ReversePipe } from './pipes/reverse.pipe';
-import { DirectiveDemoComponent } from './components/directive-demo/directive-demo.component';
-import { FilterPipe } from './pipes/filter.pipe';
+import { APP_ROUTES } from './app.routes';
 import { LoginComponent } from './components/auth/login/login.component';
-import { CommentFormComponent } from './components/users/comment-form/comment-form.component';
 import { RegisterComponent } from './components/auth/register/register.component';
-import { DataService } from './services/data.service';
-import { CounterService } from './services/counter.service';
 import { CompOneComponent } from './components/counter/comp-one/comp-one.component';
 import { CompTwoComponent } from './components/counter/comp-two/comp-two.component';
-import { PostComponent } from './components/post/post.component';
+import { DirectiveDemoComponent } from './components/directive-demo/directive-demo.component';
+import { LifeCycleDemoComponent } from './components/life-cycle-demo/life-cycle-demo.component';
+import { ObsPipeableOperatorsComponent } from './components/obs-pipeable-operators/obs-pipeable-operators.component';
+import { ObservableDemoComponent } from './components/observable-demo/observable-demo.component';
+import { PipeDemoComponent } from './components/pipe-demo/pipe-demo.component';
 import { AddPostComponent } from './components/post/add-post/add-post.component';
 import { EditPostComponent } from './components/post/edit-post/edit-post.component';
+import { PostComponent } from './components/post/post.component';
+import { CommentFormComponent } from './components/users/comment-form/comment-form.component';
+import { UserImgComponent } from './components/users/user-img/user-img.component';
+import { UserInfoComponent } from './components/users/user-info/user-info.component';
+import { UsersComponent } from './components/users/users.component';
+import { HighlightDirective } from './directives/highlight.directive';
+import { CountryCodePipe } from './pipes/country-code.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
+import { ReversePipe } from './pipes/reverse.pipe';
 import { AuthInterceptorService } from './services/auth-interceptor.service';
+import { DataService } from './services/data.service';
 import { LoggerInterceptorService } from './services/logger-interceptor.service';
-import { ObservableDemoComponent } from './components/observable-demo/observable-demo.component';
-import { ObsPipeableOperatorsComponent } from './components/obs-pipeable-operators/obs-pipeable-operators.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [         // components, directives, pipes
@@ -51,13 +53,15 @@ import { ObsPipeableOperatorsComponent } from './components/obs-pipeable-operato
     AddPostComponent,
     EditPostComponent,
     ObservableDemoComponent,
-    ObsPipeableOperatorsComponent
+    ObsPipeableOperatorsComponent,
+    HeaderComponent
   ],
   imports: [              // Module - built-in / Custom
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [
     DataService,
---
+++ users-app/src/app/app.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from "@angular/router";
+import { LoginComponent } from "./components/auth/login/login.component";
+import { RegisterComponent } from "./components/auth/register/register.component";
+import { ObservableDemoComponent } from "./components/observable-demo/observable-demo.component";
+import { PipeDemoComponent } from "./components/pipe-demo/pipe-demo.component";
+import { UsersComponent } from "./components/users/users.component";
+
+export const APP_ROUTES : Routes = [
+  {
+    path : "",
+    redirectTo : "login",
+    pathMatch : "full"
+  },{
+    path : "login",         // http://localhost:4200/login
+    component : LoginComponent
+  },{
+    path : "register",      // http://localhost:4200/register
+    component : RegisterComponent
+  },{
+    path : "users",         // http://localhost:4200/users
+    component : UsersComponent
+  }, {
+    path : "observable-demo",        // http://localhost:4200/observable-demo
+    component : ObservableDemoComponent
+  },{
+    path : "pipe-demo",         // http://localhost:4200/pipe-demo
+    component : PipeDemoComponent
+  },{
+    path : "**",                // http://localhost:4200/doesNotExist
+    redirectTo : "login",
+    pathMatch : "full"
+  }
+]
